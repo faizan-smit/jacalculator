37efad44003a84bbcdf682608d66fb6efb219a63
Muhammd Faizan Alam <121306012+faizan-smit@users.noreply.github.com>
2023-03-13 19:42:39 +0500

    Fully functional

--- app.js
+++ app.js
@@ -6,11 +6,28 @@ var inp = document.getElementById('my')
 
 }
 
-function clear(){
-    document.getElementById('my').value = ''
+function cls(){
+    document.getElementById('my').value = ' '
 }
 
 function result(){
 var inp = document.getElementById('my')
     inp.value = eval(inp.value)
+}
+
+function del1(){
+    var myarr, $1, $2, $3;
+    $1 = []
+    var inp = document.getElementById('my')
+    myarr = inp.value
+    $1 = myarr.split("");
+    $1.pop();
+    $2 = $1.join("");
+    inp.value = $2;
+    // console.log(myarr);
+    // console.log($1);
+    // console.log($2);
+    
+    
+
 }
